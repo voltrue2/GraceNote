bbca003764005ff6f7873050d4db31a55108d463
voltrue2 <voltrue2@gmail.com>
2013-05-15 18:08:32 +0900

    fix user.js language change now uses correct url for chorme and moz

--- js/cms/user.js
+++ js/cms/user.js
@@ -52,7 +52,7 @@ function createUserEditor(userData, saveMethod, languageSelector) {
 				}
 				langList.allowEvents(['change']);
 				langList.on('change', function () {
-					var url = window.location.href + '?lang=' + this.get('value');
+					var url = window.location.protocol + '//' + window.location.host + ((window.location.pathname !== '/') ? window.location.pathname : '/') + '?lang=' + this.get('value');
 					window.location.href = url;
 				});
 			} else {
